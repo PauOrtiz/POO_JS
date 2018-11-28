b970bbd7a5589a4b931012ba342fb6b23f67e9f5
Paula Ortiz <sarkofy@hotmail.com>
2018-11-28 08:51:23 -0600

    Uso de function dentro de prototype

---
+++ prototipo_a_nuevo_objeto.js
@@ -0,0 +1,7 @@
+function Dog(name) {
+     this.name = name; 
+     Dog.prototype = { //inicio de la definición del prototipo\n  
+         numLegs:2, 
+         eat:function(){//indicas que eat es una función\n\n  },\n  
+         describe:function(){//indicas que describe es una función\n\n  },//propiedades separadas por comas\n};//fin de la definición del prototipo"
+        }
